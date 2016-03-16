a59f920f0531ecd10ccce293d004f704031984bc
AndreaMinato <andrea.minato@hotmail.it>
2016-03-16 16:58:19 +0100

    Commit with GitKraken

---
+++ Test/app/src/main/java/com/example/andrea/test/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.andrea.test;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myfirstlayout);
+
+/*
+        Button piu = (Button)findViewById(R.id.plus);
+        Button meno = (Button)findViewById(R.id.minus);*/
+    }
+}
